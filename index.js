4ec6c7fbc24f5350e929b32f711313c001ff3bf8
CelinexPutte <112859814+CelinexPutte@users.noreply.github.com>
2023-02-15 13:31:53 +0100

    Eerste loop gemaakt

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 // Importeer express uit de node_modules map
-import express from 'express'
+import express from "express";
 
 // Gegevens ophalen
-const url = 'https://whois.fdnd.nl/api/v1/member/celine-van-der-putte'
-const data = await fetch(url).then((response) => response.json())
+const url =
+	"https://whois.fdnd.nl/api/v1/squad/squad-a-2022?orderBy=name&direction=ASC";
+const data = await fetch(url).then((response) => response.json());
 
-// console.log(data.member.name)
+console.log(data);
 
 // Maak een nieuwe express app aan
-const app = express()
+const app = express();
 
 // Stel ejs in als template engine en geef de 'views' map door
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 // Gebruik de map 'public' voor statische resources
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Maak een route voor de index
-app.get('/', function (req, res) {
-  // res.send('Hello World!')
-  res.render('index', data)
-})
+app.get("/", function (req, res) {
+	// res.send('Hello World!')
+	res.render("index", data);
+});
 
 // Stel het poortnummer in waar express op gaat luisteren
-app.set('port', process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 
 // Start express op, haal het ingestelde poortnummer op
-app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
-  console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+app.listen(app.get("port"), function () {
+	// Toon een bericht in de console en geef het poortnummer door
+	console.log(`Application started on http://localhost:${app.get("port")}`);
+});
